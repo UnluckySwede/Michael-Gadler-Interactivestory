736c0c514c7bd4f8371408680c3ac23daa2920e8
UnluckySwede <gadler.michael@elev.ga.ntig.se>
2021-09-28 14:45:39 +0200

    Initial commit

---
+++ Michael-Gadler-Interactivestory/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Michael_Gadler_Interactivestory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Blue Pill or Red?");
+
+            string action1 = Console.ReadLine();
+
+            Console.ReadLine();
+            if(Console = Blue)
+
+            Console.WriteLine("Noob");
+            
+        }
+    }
+}
