e8d3fae5f0b01eaa3e7e109a1a8a27010ec9d179
UnluckySwede <gadler.michael@elev.ga.ntig.se>
2021-10-06 12:28:14 +0200

    Final Product

--- Michael-Gadler-Interactivestory/Program.cs
+++ Michael-Gadler-Interactivestory/Program.cs
@@ -6,14 +6,61 @@ namespace Michael_Gadler_Interactivestory
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Blue Pill or Red?");
+            Console.WriteLine("You should on use small letters while writing your answer! Type ok if you understand.");
 
-            string action1 = Console.ReadLine();
+            String Answer = Console.ReadLine();
 
             Console.ReadLine();
-            if(Console = Blue)
+            if (Answer == "ok")
+            {
+                Console.WriteLine("What opposite of up?");
+            }
+            else
+            {
+                Console.WriteLine("kill yourself");
+                Console.ReadLine();
+            }
 
-            Console.WriteLine("Noob");
+            Answer = "yes";
+            Answer = Console.ReadLine();
+            if (Answer == "down")
+            {
+            Console.WriteLine("You're Pretty Smart, but look retarded, now you can move left or right. Choose.");
+            Answer = "placeholder";
+            Answer = Console.ReadLine();
+            if (Answer == "left" )
+            {
+            Console.WriteLine("You died! Noob, You wanna go to heaven or hell?");
+            Answer = "placeholder";
+            Answer = Console.ReadLine();
+            if (Answer == "hell"){
+                Console.WriteLine("Congrats the devil made you his bitch! Do you wanna embrace it or reject?");
+                Answer = "placeholder";
+                Answer = Console.ReadLine();
+            if (Answer == "embrace"){
+                Console.WriteLine("He takes a liking to you and grants you administration of hell.");
+            }
+            if (Answer == "reject"){
+                Console.WriteLine("He locks you in his sex dungeon for eternity, torturing you with his creative sextoys.");
+            }
+            
+            }
+            if (Answer == "heaven"){
+                Console.WriteLine("You wake upp seeing you mom at the door with belt in hand");
+                Console.ReadLine();
+            }
+            }
+            if (Answer == "right" ){
+            Console.WriteLine("You're blind so you fall of a cliff and turn into mush");
+            Console.ReadLine();
+            }
+            }
+            else
+            {
+                Console.WriteLine("gay");
+                Console.ReadLine();
+            }
+            Console.ReadLine();
             
         }
     }
